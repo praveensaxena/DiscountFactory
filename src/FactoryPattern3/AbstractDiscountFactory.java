35d417d49b0ec53acfdb56b009f0657ee029c3c0
Praveen Saxena <>
2012-03-07 21:43:46 -0600

    modified property file

--- src/FactoryPattern3/AbstractDiscountFactory.java
+++ src/FactoryPattern3/AbstractDiscountFactory.java
@@ -17,11 +17,12 @@ public class AbstractDiscountFactory {
         BackToSchool, QuantityDiscount, NODiscount, DollarDiscount
     }
     
-    private static final String FILE_NAME = "c:/Temp/discountconfig.properties";
+    private static final String FILE_NAME = "c:/temp/discountconfig.properties";
 
+    
     public static DiscountStrategy getDiscountInstance(Discounts discountType) {
         DiscountStrategy ds = null;
-
+        String disType = discountType.toString();
         // First read config setting in properties file
         File file = new File(FILE_NAME);
         Properties props = new Properties();
@@ -35,13 +36,13 @@ public class AbstractDiscountFactory {
 
             props.propertyNames();
             // Next use Java reflection to create instance
-            String className = props.getProperty(discountType.toString());
+            String className = props.getProperty(disType);
             Class clazz = Class.forName(className);
             ds = (DiscountStrategy) clazz.newInstance();
 
         } catch (Exception ex) {
             System.out.println("ERROR: you must place a copy of the\n "
-                    + "config.properties file in the 'c:/temp' directory of "
+                    + "discountconfig.properties file in the 'c:/temp' directory of "
                     + "your computer's main hard drive.");
         }
 
